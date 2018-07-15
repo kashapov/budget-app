4338d273798454a0b724339a7d2aabd54201267c
Andrey Kashapov <kashapovandrey@gmail.com>
2018-07-15 21:37:41 +0300

    make init function

--- js/app.js
+++ js/app.js
@@ -33,8 +33,22 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  
-  var DOM = UICtrl.getDomStrings();
+
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDomStrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+      //console.log(event);
+
+      if (event.keyCode === 13 || event.which === 13) {
+        //console.log('Enter was pressed.');
+        ctrlAddItem();
+      }
+    });
+  };
+
 
 
   var ctrlAddItem = function () {
@@ -43,24 +57,22 @@ var controller = (function (budgetCtrl, UICtrl) {
     // TODO:
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
+    //console.log(input);
     // 2. Add the item to the BUDGET CONTROLLER
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
 
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function (event) {
-    //console.log(event);
-
-    if (event.keyCode === 13 || event.which === 13) {
-      //console.log('Enter was pressed.');
-      ctrlAddItem();
+  return {
+    init: function () {
+      //console.log('Application has started.');
+      setupEventListeners();
     }
-  });
+  };
 
 })(budgetController, UIController);
 
+
+controller.init();
