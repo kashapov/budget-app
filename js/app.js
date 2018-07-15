7714756882ab65164d6a6e576adea56740915ca8
Andrey Kashapov <kashapovandrey@gmail.com>
2018-07-15 21:23:14 +0300

    make reading input data

--- js/app.js
+++ js/app.js
@@ -7,30 +7,59 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-  // Some code
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be income or expense
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDomStrings: function () {
+      return DOMstrings;
+    }
+  };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  // Some code
-  document.querySelector('.add__btn').addEventListener('click', function(){
-    //console.log('Button was clicked.');
+  
+  var DOM = UICtrl.getDomStrings();
+
+
+  var ctrlAddItem = function () {
+    console.log('it works.');
 
     // TODO:
     // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the BUDGET CONTROLLER
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
 
+  }
 
-  });
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-  document.addEventListener('keypress', function(event){
+  document.addEventListener('keypress', function (event) {
     //console.log(event);
 
-    
+    if (event.keyCode === 13 || event.which === 13) {
+      //console.log('Enter was pressed.');
+      ctrlAddItem();
+    }
   });
 
 })(budgetController, UIController);
