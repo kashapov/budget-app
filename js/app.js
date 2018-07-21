5526d25d455a1d815378609dfd34f25ce669a478
Andrey Kashapov <kashapovandrey@gmail.com>
2018-07-21 21:11:53 +0300

    improving the UI

--- js/app.js
+++ js/app.js
@@ -186,6 +186,12 @@ var UIController = (function () {
 
   };
 
+  var nodeListForEach = function (list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function () {
       return {
@@ -257,11 +263,7 @@ var UIController = (function () {
     displayPercentages: function (percentages) {
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function (list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
+      
 
       nodeListForEach(fields, function (current, index) {
         // to do
@@ -283,7 +285,22 @@ var UIController = (function () {
 
       year = now.getFullYear();
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
-      
+
+    },
+
+    changeType: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' + 
+        DOMstrings.inputDescription + ',' + 
+        DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function(cur){
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
     },
 
     getDomStrings: function () {
@@ -311,6 +328,8 @@ var controller = (function (budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changeType)
 
   };
 
